c06e0ca3b055f032d37c4395d06dcbe214b3e438
enkhbold470 <enkhbold470@gmail.com>
2025-01-23 03:16:03 -0800

    initialize project with Next.js, Tailwind CSS, and LiveKit integration

---
+++ components/Bigbrain.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+export default function BigBrain() {
+  return (
+    <div className="max-w-6xl mx-auto p-4 text-center">
+   
+      <h1 className="text-5xl font-bold mb-2">Unlock Your Potential with </h1>
+      <Link  className="text-blue-500 underline" href="https://da-bigbrain.github.io" target="blank">
+      Big Brain
+          </Link>
+    </div>
+  );
+}
